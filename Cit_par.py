3078dfa59d3ce5c46e5be9484a0a73a302b9afab
Menno <mennoboogaard@kpnmail.nl>
2019-03-06 10:23:05 +0100

    add gamma

--- Cit_par.py
+++ Cit_par.py
@@ -44,6 +44,7 @@ Lambda = -0.0065         # temperature gradient in ISA [K/m]
 Temp0  = 288.15          # temperature at sea level in ISA [K]
 R      = 287.05          # specific gas constant [m^2/sec^2K]
 g      = 9.81            # [m/sec^2] (gravity constant)
+gamma  = 1.4             # ratio of specific heats
 
 # air density [kg/m^3]  
 rho    = rho0 * pow( ((1+(Lambda * hp0 / Temp0))), (-((g / (Lambda*R)) + 1)))   
