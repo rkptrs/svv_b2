6b78c021a74f66fd3e3cd3a0775ed8c609ad05b7
Menno <mennoboogaard@kpnmail.nl>
2019-03-06 17:10:16 +0100

    Add elevatortrimcurve

--- Data regression.py
+++ Data regression.py
@@ -2,7 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 import os
+import scipy
+import scipy.stats
 from Cit_par import rho0,S,Gamma,Lambda,Temp0,p0,g,R,A,e
+
+CNalpha=False
+CN_CT=False
+elevatortrimcurve=True
 
 ##k=20
 ##x=np.arange(0,1.5,1/k)
@@ -13,6 +19,7 @@ from Cit_par import rho0,S,Gamma,Lambda,Temp0,p0,g,R,A,e
 
 
 def polyfitter(x1,y1,degree):
+    outlierchecks=5
     coefficients=np.flip(np.polyfit(x1,y1,degree))
     y=0
     i=0
@@ -21,11 +28,37 @@ def polyfitter(x1,y1,degree):
     for cff in coefficients:
         y+=cff*x**i
         i+=1
-    return x,y,coefficients
+
+
+    for j in range(outlierchecks):
+        ycheck=0
+        i=0
+        for cff in coefficients:
+            ycheck+=cff*x1**i
+            i+=1
+        e=y1-ycheck
+        threshold=2
+        deleters=np.array([])
+        for i in range(len(e)):
+            if abs(scipy.stats.zscore(e)[i])>threshold:
+                x1=np.delete(x1,i)
+                y1=np.delete(y1,i)
+                print ('oulier:',i)
+                break
+    
+    coefficients=np.flip(np.polyfit(x1,y1,degree))
+    y=0
+    i=0
+    step=(max(x1)-min(x1))/2/len(x1)
+    x=np.arange(min(x1),max(x1)+step,step)
+    for cff in coefficients:
+        y+=cff*x**i
+        i+=1
+    return x1,y1,x,y,coefficients
 
 
 
-
+#1st stationairy CL-Cd
 h=np.array([7000,7000,6990,6990,6900,7010]) #ft
 V=np.array([251,220,190,163,143,117]) #kts
 TAT=np.array([2.5,0.8,-0.8,-3.0,-3.2,-4.2]) #degree C
@@ -34,6 +67,10 @@ FFr=np.array([842.,690.,552.,492.,422.,447.]) #lbs/hr
 Wf=np.array([370,421,456,493,524,552]) #lbs
 Winit=14064.765428 #lbs
 alpha=np.array([1.4,2.1,3.3,4.9,6.6,10.3]) #degree
+
+#stationairy elevator trim
+alpha=np.array([5.3,6.3,7.3,8.5,4.5,4.1,3.4]) #degree
+de=np.array([0,-0.4,0.9,-1.5,0.4,0.6,1.0]) #degree
 
 
 #convert to SI
@@ -56,15 +93,6 @@ TmomISA=Temp0+Lambda*hmeters
 Tdiff=Tmom-TmomISA
 
 CN=Wmom/(0.5*rho0*V**2*S)
-
-degree=1
-
-#plt.plot(alpha,CN,'o')
-#plt.plot(polyfitter(alpha,CN,degree)[0],polyfitter(alpha,CN,degree)[1])
-#plt.show()
-
-#print(polyfitter(alpha,CN,degree)[2])
-
 data=np.stack((h,M,Tdiff,FFl,FFr)).T
 np.savetxt('matlab.dat',data,delimiter=' ')
 
@@ -79,12 +107,32 @@ for line in T:
 
 CT=D/(0.5*rho0*V**2*S)
 
-degree=2
-plt.plot(CT,CN,'o')
-plt.plot(polyfitter(CN,CT,degree)[1],polyfitter(CN,CT,degree)[0])
+degree=1
+if CNalpha:
+    polyfits=polyfitter(alpha,CN,degree)
+    plt.plot(polyfits[0],polyfits[1],'o')
+    plt.plot(polyfits[2],polyfits[3])
+
+    print(polyfits[4])
+
+
+
+if CN_CT:
+    polyfits=polyfitter(CN,CT,degree)
+    degree=2
+    plt.plot(polyfits[1],polyfits[0],'o')
+    plt.plot(polyfits[2],polyfits[3])
+
+    print('lift drag polar constants: Cd0,k,1/(Pi*A*e)', polyfits[4])
+    print(1/A/e/pi)
+
+if elevatortrimcurve:
+    polyfits=polyfitter(alpha,de,degree)
+    degree=1
+    plt.plot(polyfits[0],polyfits[1],'o')
+    plt.plot(polyfits[2],polyfits[3])
+    plt.gca().invert_yaxis()
+    print ('-Cmalpha/Cmdelta=',polyfits[4][1])
 plt.show()
 
-print('lift drag polar constants: Cd0,k,1/(Pi*A*e)', polyfitter(CN,CT,degree)[2])
 
-
-print(1/A/e/pi)
