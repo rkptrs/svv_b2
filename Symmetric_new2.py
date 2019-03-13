d2506b5223e66a47a1be1a0304d5cc203127adc8
Bas <BasvManen@Gmail.com>
2019-03-13 10:05:50 +0100

    Added test file for symmetric

--- Symmetric_new2.py
+++ Symmetric_new2.py
@@ -1,4 +1,4 @@
-from Cit_par import *
+from Cit_par_test import *
 import numpy as np
 import control.matlab as cmat
 import matplotlib.pyplot as plt
@@ -19,13 +19,13 @@ import matplotlib.pyplot as plt
 # q
 
 # Parameters for Matrix C1
-C111 = -2 * muc 
+C111 = -2 * (c/V0**2) * muc 
 C112 = 0
 C113 = 0
 C114 = 0
 
 C121 = 0
-C122 = V0 * (CZadot - 2*muc)
+C122 = (c/V0) * (CZadot - 2*muc)
 C123 = 0
 C124 = 0
 
@@ -35,9 +35,9 @@ C133 = -1
 C134 = 0
 
 C141 = 0
-C142 = V0 * Cmadot
+C142 = (c/V0) * Cmadot
 C143 = 0
-C144 = -2 * muc * KY2 * c
+C144 = -2 * muc * KY2 * (c**2/V0**2)
 
 C1 = np.matrix([[C111, C112, C113, C114],
                 [C121, C122, C123, C124],
@@ -45,25 +45,25 @@ C1 = np.matrix([[C111, C112, C113, C114],
                 [C141, C142, C143, C144]])
 
 # Parameters for Matrix C2
-C211 = (V0/c) * CXu
-C212 = (V0**2/c) * CXa
-C213 = (V0**2/c) * CZ0
-C214 = V0 * CXq
+C211 = (1/V0) * CXu
+C212 = CXa
+C213 = CZ0
+C214 = (c/V0) * CXq
 
-C221 = (V0/c) * CZu
-C222 = (V0**2/c) * CZa
-C223 = -(V0**2/c) * CX0
-C224 = V0 * (CZq + 2*muc)
+C221 = (1/V0) * CZu
+C222 = CZa
+C223 = - CX0
+C224 = (c/V0) * (CZq + 2*muc)
 
 C231 = 0
 C232 = 0
 C233 = 0
 C234 = 1
 
-C241 = (V0/c) * Cmu
-C242 = (V0**2/c) * Cma
+C241 = (1/V0) * Cmu
+C242 = Cma
 C243 = 0
-C244 = V0 * Cmq
+C244 = (c/V0) * Cmq
 
 C2 = np.matrix([[C211, C212, C213, C214],
                 [C221, C222, C223, C224],
@@ -71,10 +71,10 @@ C2 = np.matrix([[C211, C212, C213, C214],
                 [C241, C242, C243, C244]])
 
 # Parameters for Matrix C3
-C311 = (V0**2/c) * CXde
-C321 = (V0**2/c) * CZde
+C311 = CXde
+C321 = CZde
 C331 = 0
-C341 = (V0**2/c) * Cmde
+C341 = Cmde
 
 C3 = np.matrix([[C311],
                 [C321],
@@ -97,8 +97,8 @@ D = np.zeros((4,1))
 sys = cmat.ss(A, B, C, D)
 
 T = np.linspace(0, 150, 1000)
-X0 = [V0, alpha0, th0, 0]
-U = np.zeros(len(T))
+X0 = [V0, 0, 0, 0]
+U = np.ones(len(T))*-0.005
 
 # Generate Outputs
 yout, T, xout = cmat.lsim(sys, U=U, T=T, X0=X0)
@@ -109,6 +109,6 @@ theta = yout[:,2]
 q = yout[:,3]
 
 # Print Eigenvalues and Plot Graphs
-plt.plot(T, u)
+plt.plot(T, alpha)
 plt.show()
 
