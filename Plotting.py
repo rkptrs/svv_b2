031bc7e2ed7df9641a267f7778882fbdd7c43461
Bas <BasvManen@Gmail.com>
2019-03-15 11:44:52 +0100

    Fixed Github for me

--- Plotting.py
+++ Plotting.py
@@ -32,7 +32,7 @@ def givedata(choosedemo, chooseparam, plotting): #function that will return flig
     meastimes1 = ['00:20:10', '00:21:40', '00:23:40', '00:26:30', '00:28:45', '00:31:00'] #times of measurements for series 1
     meastimes2 = ['00:36:00', '00:37:30', '00:39:35', '00:41:10', '00:43:05', '00:44:20', '00:45:25', '00:46:57', '00:48:52'] #times of measurement for series 2
     meas2cgshift = [False, False, False, False, False, False, False, True, True] #for which measurements in series 2 was the cg shifted
-    meastimesdemo = ['00:50:07', '00:51:24', '00:52:30', '00:55:53', '00:57:30', '01:00:02', '01:04:00']#measurement times for the demonstration part
+    meastimesdemo = ['00:50:07', '00:51:24', '00:52:34', '00:55:53', '00:57:30', '01:00:02', '01:04:00']#measurement times for the demonstration part
     measdemotype = ['Short period', 'Aperiodic Roll', 'Phugoid', 'Dutch Roll', 'Dutch Roll with YD', 'Spiral', 'Parabola'] #which type of manoeuvre was flown
 
     #Entering available parameters
@@ -99,7 +99,7 @@ def givedata(choosedemo, chooseparam, plotting): #function that will return flig
         
     return chosentime, outputdata
 
-time, data = givedata('Phugoid', ['aoa','pitchangle','pitchrate','tas'], True)
+#time, data = givedata('Phugoid', ['aoa','pitchangle','pitchrate','tas'], True)
 
 """ Putting lines into the full time-scale plots
 #adding timestamps of series 1 in black
