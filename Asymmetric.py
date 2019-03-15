fd1873b9d546683810fe9df71d6fac49a35dfb82
MaartenvNistelrooij <maarten.vnistelrooij@gmail.com>
2019-03-15 11:50:45 +0100

    Nog een keer

--- Asymmetric.py
+++ Asymmetric.py
@@ -6,29 +6,29 @@ from Cit_par import *
 
 #define initial paramaters
 
-#V0 = 59.9
-#S = 24.2
-#b = 13.36
-#CL = 1.136
-#mub = 15.5
-#KX2 = 0.012
-#KZ2 = 0.037
-#KXZ = 0.002
-#CYb = -0.9896
-#CYp = -0.0870
-#CYr = 0.4300
-#CYda = 0
-#CYdr = 0.3037
-#Clb = -0.0772
-#Clp = -0.3444
-#Clr = 0.2800
-#Clda = -0.2349
-#Cldr = 0.0286
-#Cnb = 0.1638
-#Cnp = -0.0108
-#Cnr = -0.1930
-#Cnda = 0.0286
-#Cndr = -0.1261
+V0 = 59.9
+S = 24.2
+b = 13.36
+CL = 1.136
+mub = 15.5
+KX2 = 0.012
+KZ2 = 0.037
+KXZ = 0.002
+CYb = -0.9896
+CYp = -0.0870
+CYr = 0.4300
+CYda = 0
+CYdr = 0.3037
+Clb = -0.0772
+Clp = -0.3444
+Clr = 0.2800
+Clda = -0.2349
+Cldr = 0.0286
+Cnb = 0.1638
+Cnp = -0.0108
+Cnr = -0.1930
+Cnda = 0.0286
+Cndr = -0.1261
 
 V = V0
 
@@ -133,7 +133,7 @@ Unew = np.zeros((Trng,2))
 for i in range(Trng):
     Unew[i][1] = deltar[rng[0]+i]
     Unew[i][0] = deltaa[rng[0]+i]
-sys = cmat.ss(A_asym, B_asym, C_asym, D_asym)
+##sys = cmat.ss(A_asym, B_asym, C_asym, D_asym)
 sys2 = cmat.ss(A_asym2, B_asym2, C_asym2, D_asym2)
 
 #test1 = control.impulse_response(sys, T=T, input = 0)
